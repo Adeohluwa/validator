561d959eb25f9a71691832fd095260b032552963
Adeoluwa Adejumo <adejumoadeoluwa@gmail.com>
2019-12-22 23:01:42 +0100

    Create main module

---
+++ src/validator.nim
@@ -0,0 +1,23 @@
+import validator/[isEmail]
+
+# [
+#isEmail
+#isIP
+#isISBN
+#isPhone
+#isDate
+#isCurrency
+#isCreditCard
+#isEmpty
+#isHash
+#isJson
+#isJwt
+#isLatLong
+#isPort
+#isUrl
+#isPostalCode
+#
+# ]#
+
+
+export isEmail
