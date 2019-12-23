5f99b21a1d77035e257eaefe52f4a357250e7a66
Adeoluwa Adejumo <adejumoadeoluwa@gmail.com>
2019-12-23 19:49:59 +0100

    Ensure email has domain

--- src/validator/isEmail.nim
+++ src/validator/isEmail.nim
@@ -1,3 +1,4 @@
+import re
 import sugar
 import strutils
 import sequtils
@@ -11,7 +12,9 @@ let hasAtSign = (text: string) => text.contains "@"
 
 let excludeInvalidSymbols = (text: string) => not text.contains {'&','=','\'','+','<','>','-','/','\\',' '}
 
-#lethasDotmain = (text: string) => text notin {".com"}
+let domains = re"@[A-Za-z]+\.[A-Za-z]+"
+
+let hasDomain = (text: string) => text.findAll(domains).len() != 0
 
 let noDoubleDots = (text: string) => not text.contains ".."
 
@@ -21,10 +24,11 @@ let notStartWithNumbers = (text: string) => text[0] notin {'0'..'9'}
 
 
 
-proc isEmail*(text: string): bool =
-  if text.hasAtSign:
-    if text.excludeInvalidSymbols:
-      if text.noDoubleDots:
-        if text.notStartWithNumbers:
-          return true
+proc isEmail*(entry: string): bool =
+  if entry.hasAtSign:
+    if entry.hasDomain:
+      if entry.excludeInvalidSymbols:
+        if entry.noDoubleDots:
+          if entry.notStartWithNumbers:
+            return true
   return false
