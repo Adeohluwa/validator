dc19965d0fe104d17a94bc9c53da1d84fa6a82f2
Adeoluwa Adejumo <adejumoadeoluwa@gmail.com>
2019-12-23 19:11:02 +0100

    Ensure @ sign, no invalid symbols, no double dots, no starting numbers

--- src/validator/isEmail.nim
+++ src/validator/isEmail.nim
@@ -1,2 +1,30 @@
-func isEmail*(email: string): bool =
-  return true
+import sugar
+import strutils
+import sequtils
+
+
+
+
+
+#let underMaxLength = 64
+let hasAtSign = (text: string) => text.contains "@"
+
+let excludeInvalidSymbols = (text: string) => not text.contains {'&','=','\'','+','<','>','-','/','\\',' '}
+
+#lethasDotmain = (text: string) => text notin {".com"}
+
+let noDoubleDots = (text: string) => not text.contains ".."
+
+let notStartWithNumbers = (text: string) => text[0] notin {'0'..'9'}
+
+
+
+
+
+proc isEmail*(text: string): bool =
+  if text.hasAtSign:
+    if text.excludeInvalidSymbols:
+      if text.noDoubleDots:
+        if text.notStartWithNumbers:
+          return true
+  return false
