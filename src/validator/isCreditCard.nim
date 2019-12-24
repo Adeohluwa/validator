e4acd9f070398b7270e1b47caa4a762a2484f00a
Adeoluwa Adejumo <adejumoadeoluwa@gmail.com>
2019-12-24 22:13:45 +0100

    Add all credit card patterns

--- src/validator/isCreditCard.nim
+++ src/validator/isCreditCard.nim
@@ -4,32 +4,38 @@ import strutils
 
 
 
-let patterns = @[""]
-
-let mastercard = (text: string) => true and text.len == 16
-
-let visa = (text: string) => true and text.len == 16
-
-let americanExpress = (text: string) => true and text.len == 15
-
-let discover = (text: string) => true and text.len == 16
-
-let JCB = (text: string) => true and text.len == 16
-
-let diners = (text: string) => true and text.len == 14
-
-let maestro = (text: string) => true and text.len >= 13 and text.len <= 19
-
-let allChecksPassed = (text: string) => text.mastercard or text.visa or text.americanExpress or
-text.discover or text.JCB or text.diners or text.maestro
+let patterns = @[re"^5[1-5]\d{14}$", re"^3[47][0-9]{13}$", re"^4[0-9]{12}(?:[0-9]{3})?$", re"^65[4-9][0-9]{13}|64[4-9][0-9]{13}|6011[0-9]{12}|(622(?:12[6-9]|1[3-9][0-9]|[2-8][0-9][0-9]|9[01][0-9]|92[0-5])[0-9]{10})$",re"^(?:2131|1800|35[0-9]{3})[0-9]{11}$", re"^3(?:0[0-5]|[68][0-9])[0-9]{11}$", re"^(5018|5081|5044|5020|5038|603845|6304|6759|676[1-3]|6799|6220|504834|504817|504645)[0-9]{8,15}$"]
 
 
 
+let exists = (text: seq[string]) => text.len() != 0
 
-proc isCreditCard*(entry: string): bool = entry.allChecksPassed()
+let mastercard = (text: string) => text.findAll(patterns[0]).exists
+
+let americanExpress = (text: string) => text.findAll(patterns[1]).exists
+
+let visa = (text: string) => text.findAll(patterns[2]).exists
+
+let discover = (text: string) => text.findAll(patterns[3]).exists
+
+let JCB = (text: string) => text.findAll(patterns[4]).exists
+
+let diners = (text: string) => text.findAll(patterns[5]).exists
+
+let maestro = (text: string) => text.findAll(patterns[6]).exists
+
+let testCard = (text: string) => text.mastercard or text.visa or text.americanExpress or text.discover or text.JCB or text.diners or text.maestro
+
+
+
+
+proc isCreditCard*(entry: string): bool = entry.testCard()
 
 
 
 when defined(test):
-  assert isCreditCard("5555555555555555")
+  assert isCreditCard("5555444455554444") == true
+  assert isCreditCard("378282246310005")  == true
+  assert isCreditCard("4242424242424242") == true
+  assert isCreditCard("6011111111111117") == true
   echo "Tests Successful"
