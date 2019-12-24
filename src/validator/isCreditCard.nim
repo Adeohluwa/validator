b725d2fd6924d172117d847eb92d4fc903f1f03f
Adeoluwa Adejumo <adejumoadeoluwa@gmail.com>
2019-12-24 23:37:45 +0100

    Change variable name to card

--- src/validator/isCreditCard.nim
+++ src/validator/isCreditCard.nim
@@ -1,32 +1,33 @@
 import re
 import sugar
 import strutils
+#import unpack
+
+
+let patterns = (re"^5[1-5]\d{14}$", re"^3[47][0-9]{13}$", re"^4[0-9]{12}(?:[0-9]{3})?$", re"^65[4-9][0-9]{13}|64[4-9][0-9]{13}|6011[0-9]{12}|(622(?:12[6-9]|1[3-9][0-9]|[2-8][0-9][0-9]|9[01][0-9]|92[0-5])[0-9]{10})$",re"^(?:2131|1800|35[0-9]{3})[0-9]{11}$", re"^3(?:0[0-5]|[68][0-9])[0-9]{11}$", re"^(5018|5081|5044|5020|5038|603845|6304|6759|676[1-3]|6799|6220|504834|504817|504645)[0-9]{8,15}$")
+let (mcs, amex, vs, dsc, jc, dns, mst) = patterns
 
 
 
-let patterns = @[re"^5[1-5]\d{14}$", re"^3[47][0-9]{13}$", re"^4[0-9]{12}(?:[0-9]{3})?$", re"^65[4-9][0-9]{13}|64[4-9][0-9]{13}|6011[0-9]{12}|(622(?:12[6-9]|1[3-9][0-9]|[2-8][0-9][0-9]|9[01][0-9]|92[0-5])[0-9]{10})$",re"^(?:2131|1800|35[0-9]{3})[0-9]{11}$", re"^3(?:0[0-5]|[68][0-9])[0-9]{11}$", re"^(5018|5081|5044|5020|5038|603845|6304|6759|676[1-3]|6799|6220|504834|504817|504645)[0-9]{8,15}$"]
+let exists = (matches: seq[string]) => matches.len() != 0
+
+let mastercard = (card: string) => card.findAll(mcs).exists
+
+let americanExpress = (card: string) => card.findAll(amex).exists
+
+let visa = (card: string) => card.findAll(vs).exists
+
+let discover = (card: string) => card.findAll(dsc).exists
+
+let JCB = (card: string) => card.findAll(jc).exists
+
+let diners = (card: string) => card.findAll(dns).exists
+
+let maestro = (card: string) => card.findAll(mst).exists
+
+let testCard = (card: string) => card.mastercard or card.visa or card.americanExpress or
+card.discover or card.JCB or card.diners or card.maestro
 
 
 
-let exists = (text: seq[string]) => text.len() != 0
-
-let mastercard = (text: string) => text.findAll(patterns[0]).exists
-
-let americanExpress = (text: string) => text.findAll(patterns[1]).exists
-
-let visa = (text: string) => text.findAll(patterns[2]).exists
-
-let discover = (text: string) => text.findAll(patterns[3]).exists
-
-let JCB = (text: string) => text.findAll(patterns[4]).exists
-
-let diners = (text: string) => text.findAll(patterns[5]).exists
-
-let maestro = (text: string) => text.findAll(patterns[6]).exists
-
-let testCard = (text: string) => text.mastercard or text.visa or text.americanExpress or text.discover or text.JCB or text.diners or text.maestro
-
-
-
-
-proc isCreditCard*(entry: string): bool = entry.testCard()
+proc isCreditCard*(card: string): bool = card.testCard()
